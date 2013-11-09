5264f2094168e97ec725abb09d1658b75961acec
Kees Bakker <kees@sodaq.net>
2013-11-09 16:32:23 +0100

    Reformat the function OWtemp using Eclipse

--- SODAQ_beer/SODAQ_beer.ino
+++ SODAQ_beer/SODAQ_beer.ino
@@ -60,33 +60,31 @@ void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
  
 
 
-float OWtemp(void){
 
+float OWtemp(void)
+{
   byte i;
   byte present = 0;
   byte type_s;
   byte data[12];
-  byte addr[8]="";
+  byte addr[8] = "";
   float celsius;
 
   ds.search(addr);
   ds.reset();
   ds.select(addr);
-  ds.write(0x44, 1);        // start conversion, with parasite power on at the end
-  
-  delay(1000);     // maybe 750ms is enough, maybe not
+  ds.write(0x44, 1);                    // start conversion, with parasite power on at the end
+
+  delay(1000);                          // maybe 750ms is enough, maybe not
   // we might do a ds.depower() here, but the reset will take care of it.
-  
+
   present = ds.reset();
-  ds.select(addr);    
-  ds.write(0xBE);         // Read Scratchpad
+  ds.select(addr);
+  ds.write(0xBE);                       // Read Scratchpad
 
-
-  for ( i = 0; i < 9; i++) {           // we need 9 bytes
+  for (i = 0; i < 9; i++) {             // we need 9 bytes
     data[i] = ds.read();
-
   }
-
 
   // Convert the data to actual temperature
   // because the result is a 16 bit signed integer, it should
@@ -102,14 +100,16 @@ float OWtemp(void){
   } else {
     byte cfg = (data[4] & 0x60);
     // at lower res, the low bits are undefined, so let's zero them
-    if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
-    else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
-    else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+    if (cfg == 0x00)
+      raw = raw & ~7; // 9 bit resolution, 93.75 ms
+    else if (cfg == 0x20)
+      raw = raw & ~3; // 10 bit res, 187.5 ms
+    else if (cfg == 0x40)
+      raw = raw & ~1; // 11 bit res, 375 ms
     //// default is 12 bit resolution, 750 ms conversion time
   }
-  celsius = (float)raw / 16.0;
+  celsius = (float) raw / 16.0;
   return celsius;
 }
  
 /* ( THE END ) */
-
